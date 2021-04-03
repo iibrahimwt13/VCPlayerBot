a4d3e2d3a013a9e176a25f86ce9e8f3a4c08b563
sangramghangale <81154807+sangramghangale@users.noreply.github.com>
2021-04-03 18:53:33 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -10,10 +10,10 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 )
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name}!
-I am 𝗩𝗖𝗣𝗹𝗮𝘆𝗕𝗼𝘁 VC Music Player, an open-source bot that lets you play music in your Telegram groups.
-Maintained by @HEROGAMERS1 ❤
-For source code Join our support group @LaylaSupport.
+        f"""<b>Hello🥳 {message.from_user.first_name}!
+I am 𝗖𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝘆𝗤𝘂𝗲𝗲𝗻𝗕𝗼𝘁 VC Music Player, an close-source bot that lets you play music in your Telegram groups.
+Maintained by @sangramghangale ❤
+For source code Join our support group @maharashtrafriends.
 Use the buttons below to know more about me.
  </b>""",
       
@@ -22,20 +22,20 @@ Use the buttons below to know more about me.
             [
                 [
                     InlineKeyboardButton(
-                        "Command", url="https://telegra.ph/ĦŘØ-GΔΜŘ-04-02",
+                        "Command", url="https://telegra.ph/%F0%9D%97%96%F0%9D%97%B5%F0%9D%97%BC%F0%9D%97%B0%F0%9D%97%BC%F0%9D%97%B9%F0%9D%97%AE%F0%9D%98%81%F0%9D%98%86%F0%9D%97%A4%F0%9D%98%82%F0%9D%97%B2%F0%9D%97%B2%F0%9D%97%BB%F0%9D%97%95%F0%9D%97%BC%F0%9D%98%81-04-03",
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "👥 Group", url="https://t.me/LaylaSupport"
+                        "👥 Group", url="https://t.me/maharashtrafriendcircle"
                     ),
                     InlineKeyboardButton(
-                        "💾 Source code", url="https://github.com/QueenArzoo/VCPlayBot"
+                        "💾 Source code", url="https://t.me/chocolatyqueenvcplayer"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Join ChatGroup", url="https://t.me/GIRLS_AND_BOYS_CHATTING"
+                        "Join ChatGroup", url="https://t.me/digestnotes"
                     )
                 ]
             ]
