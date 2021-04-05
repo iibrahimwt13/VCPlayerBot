b1cbbc731e6af340234631a8dda249490a197bb5
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-04-06 01:18:28 +0300

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -16,7 +16,7 @@ async def search(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Type a YouTube video name...",
+            switch_pm_text="YouTube video adı yazın...",
             switch_pm_parameter="help",
             cache_time=0
         )
@@ -49,6 +49,6 @@ async def search(client: Client, query: InlineQuery):
             await query.answer(
                 results=answers,
                 cache_time=0,
-                switch_pm_text="Error: Search timed out",
+                switch_pm_text="Error: Arama zaman Aşımına Uğradı",
                 switch_pm_parameter="",
             )
